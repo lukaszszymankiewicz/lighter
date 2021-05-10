8e1853f11389f9037e9b262ae04d8aeb7772f31c
lukaszszymankiewicz <lukasz_szymankiewicz@op.pl>
2021-05-10 10:11:51 +0200

    first sprite shown

--- Makefile
+++ Makefile
@@ -1,15 +1,13 @@
 CC = gcc
 CFLAGS := --std=c99 -Wall 
-LIBS = `pkg-config --cflags --libs sdl2` 
+LINKS = `pkg-config --cflags --libs sdl2` 
 TARGET = lighter
 
 all:
 	$(TARGET)
 
 $(TARGET): $(TARGET).c
-	$(CC) $(CFLAGS) $(TARGET).c -o $(TARGET) $(LIBS)
+	$(CC) $(CFLAGS) $(TARGET).c -o $(TARGET) $(LINKS)
 
 clean:
 	$(RM) $(TARGET)
-
-# gcc --std=c99 -Wall lighter.c -o lighter `pkg-config --cflags --libs sdl2`
--- lighter.c
+++ lighter.c
@@ -1,17 +1,57 @@
 #include <SDL2/SDL.h>
  
-#define W 640
-#define H 480
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
-int main(int argc, char *argv[]){
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-      printf("error initializing SDL: %s\n", SDL_GetError());
-  }
+SDL_Window* window = NULL;
+SDL_Surface* surface = NULL;
+SDL_Surface* player_frame = NULL;
 
-  SDL_Window* win = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, W, H, 0);
+int init_game(){
+    if(SDL_Init( SDL_INIT_VIDEO ) < 0) {
+        printf("Fatal Error!");
+        return 0;
+    }
 
-  while (1)
-      ;
- 
-  return 0;
+    else {
+        window = SDL_CreateWindow(
+           "Lighter",
+           SDL_WINDOWPOS_UNDEFINED,
+           SDL_WINDOWPOS_UNDEFINED,
+           SCREEN_WIDTH,
+           SCREEN_HEIGHT,
+           SDL_WINDOW_SHOWN
+        );
+        surface = SDL_GetWindowSurface(window);
+    }
+    return 1;
+}
+
+int load_frames() {
+    player_frame = SDL_LoadBMP("player.bmp");
+
+    if(player_frame == NULL) {
+        printf("unable to locate image!");
+        return 0;
+    }
+    printf("All frames loaded sucesfully!");
+    return 1;
+}
+
+void close_game() {
+    SDL_FreeSurface( player_frame );
+    player_frame = NULL;
+    SDL_DestroyWindow( window );
+    window = NULL;
+    SDL_Quit();
+}
+
+int main( int argc, char* args[] ) {
+    init_game();
+    load_frames();
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFA, 0xFF));
+    SDL_BlitSurface(player_frame, NULL, surface, NULL);
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
+    close_game();
 }
