d12157af197a6877eead22022c4ffc04ce3679a3
lukaszszymankiewicz <lukasz_szymankiewicz@op.pl>
2021-09-19 17:07:44 +0200

    almostetst there

--- src/config.h
+++ src/config.h
@@ -13,14 +13,14 @@
 #define SCREEN_HEIGHT          200                // screen height in pixels
 #define MAX_KEYS               SDL_NUM_SCANCODES  // SDL max keys supported
 
-#define DEFAULT_DARK_R         0
-#define DEFAULT_DARK_G         0
-#define DEFAULT_DARK_B         0
-#define DEFAULT_DARK_A         100
+#define DEFAULT_DARK_R         10
+#define DEFAULT_DARK_G         10
+#define DEFAULT_DARK_B         10
+#define DEFAULT_DARK_A         50
 
 #define DEFAULT_LIGHT_R        169
 #define DEFAULT_LIGHT_G        169
-#define DEFAULT_LIGHT_B        169
+#define DEFAULT_LIGHT_B        92
 #define DEFAULT_LIGHT_A        20
 
 #define GAME_NAME              "Lighter"
